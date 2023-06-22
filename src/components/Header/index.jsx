a53e94c02a4e71e216b87ecf38aa42f51ef93661
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-06-22 09:54:00 +0500

    set header contents

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { Container, OuterContainer } from "./style";
+import { Container, HashTag, HashTagsWrapper, HeaderText, HeaderTitle, MoreContentLink, OuterContainer, SignUpButton } from "./style";
 
 function Header() {
   return (
     <OuterContainer>
-      <Container>Header</Container>
+      <Container>
+        <HeaderTitle>
+          Bizning maqsadimiz oʻzbek ilmiy kontentini boyitish
+        </HeaderTitle>
+        <HeaderText>
+          Bilimingizni maqolalarda ulashing va hamjamiyatni kengaytiring
+        </HeaderText>
+        <SignUpButton>Hamjamiyatga qo’shiling</SignUpButton>
+        <HashTagsWrapper>
+          <HashTag>#dolzarb</HashTag>
+          <HashTag>#ta’lim</HashTag>
+          <HashTag>#iqtisodiyot</HashTag>
+          <HashTag>#madaniyat</HashTag>
+          <HashTag>#texnologiya</HashTag>
+          <HashTag>#san’at</HashTag>
+        </HashTagsWrapper>
+        <MoreContentLink href="#">ko‘proq mavzularni ko‘rish</MoreContentLink>
+      </Container>
     </OuterContainer>
   );
 }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { globalContainer } from "../../root/global";
+import { globalContainer, signUpButton } from "../../root/global";
 
 export const OuterContainer = styled.div`
   background-color: var(--light-blue);
@@ -8,4 +8,49 @@ export const OuterContainer = styled.div`
 export const Container = styled.div`
   ${globalContainer};
   margin-inline: auto;
+  padding: 48px 0;
+`;
+
+export const HeaderTitle = styled.h1`
+  color: #3a3a3a;
+  font-size: 64px;
+  font-family: Mountella;
+  max-width: 824px;
+  margin-bottom: 16px;
+`;
+
+export const HeaderText = styled.p`
+  color: #3a3a3a;
+  font-size: 20px;
+  font-family: Inter;
+  line-height: 28px;
+  margin-bottom: 40px;
+`;
+
+export const SignUpButton = styled.button`
+  ${signUpButton}
+  margin-bottom: 32px;
+`;
+
+export const HashTagsWrapper = styled.div`
+  display: flex;
+`;
+
+export const HashTag = styled.span`
+  color: #1c1c1c;
+  font-size: 16px;
+  font-family: Inter;
+  line-height: 24px;
+  &:not(:last-child) {
+    margin-right: 24px;
+  }
+  margin-bottom: 36px;
+`;
+
+export const MoreContentLink = styled.a`
+  color: #0085ff;
+  text-align: center;
+  font-size: 16px;
+  font-family: Inter;
+  line-height: 24px;
 `;
