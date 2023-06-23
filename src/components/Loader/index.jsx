217fa04e228e3364dceac4780916c4c28980fbb0
topilovasilbek <asilbektback@gmail.com>
2023-06-23 15:48:05 +0500

    loading component added

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Container, OuterContainer } from "./style";
+import { ReactComponent as Image } from "../../assets/images/loading.svg";
 
 function Loader() {
   return (
     <OuterContainer>
-      <Container>Loading...</Container>
+      <Container>
+        <Image />
+      </Container>
     </OuterContainer>
   );
 }
--- src/components/Loader/style.js
+++ src/components/Loader/style.js
@@ -1,9 +1,23 @@
 import styled from "styled-components";
 import { globalContainer } from "../../root/global";
 
-export const OuterContainer = styled.div``;
+export const OuterContainer = styled.div`
+  min-height: 100vh;
+  padding-block: 20px;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+`;
 
 export const Container = styled.div`
   ${globalContainer};
   margin-inline: auto;
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    max-width: 25%;
+  }
 `;
