78f77d2ec4d8bf0416b9fc724ab3427e4bc47cbc
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-06-23 22:20:35 +0500

    Finish mobile download

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -4,9 +4,19 @@ import {
   AboutContentBtn,
   AboutContentText,
   AboutContentWrap,
+  CarouserSectionWrapper,
   Container,
+  DownBtnApple,
+  DownBtnPlay,
   HeaderBgImg,
   HeaderBgImgMobile,
+  MobileAppContent,
+  MobileAppImg,
+  MobileAppInner,
+  MobileAppWrapper,
+  MobileBtns,
+  MobileText,
+  MobileTitle,
   OuterContainer,
   UzbMap,
   UzbMapWrap,
@@ -15,25 +25,45 @@ import Navbar from "../Navbar/index";
 
 function About() {
   return (
-    <OuterContainer>
-      <HeaderBgImgMobile />
-      <Navbar yellowbg="true" />
-      <Header />
-      <Container>
-        <UzbMapWrap>
-          <UzbMap />
-        </UzbMapWrap>
-        <AboutContentWrap>
-          <AboutContentText>
-            O'zbeklar yangi yilni yangi yil deb nomlangan bayram bilan
-            nishonlaydilar. Ular yangi yil daraxtini bezashadi, yangi yil
-            bayramini nishonlashadi va bir-birlariga sovg'alar berishadi.
-          </AboutContentText>
-          <AboutContentBtn>Siz ham bilim ulashing</AboutContentBtn>
-        </AboutContentWrap>
-      </Container>
-      <HeaderBgImg />
-    </OuterContainer>
+    <>
+      <OuterContainer>
+        <HeaderBgImgMobile />
+        <Navbar yellowbg="true" />
+        <Header />
+        <Container>
+          <UzbMapWrap>
+            <UzbMap />
+          </UzbMapWrap>
+          <AboutContentWrap>
+            <AboutContentText>
+              O'zbeklar yangi yilni yangi yil deb nomlangan bayram bilan
+              nishonlaydilar. Ular yangi yil daraxtini bezashadi, yangi yil
+              bayramini nishonlashadi va bir-birlariga sovg'alar berishadi.
+            </AboutContentText>
+            <AboutContentBtn>Siz ham bilim ulashing</AboutContentBtn>
+          </AboutContentWrap>
+          <CarouserSectionWrapper></CarouserSectionWrapper>
+        </Container>
+        <HeaderBgImg />
+      </OuterContainer>
+      <MobileAppWrapper>
+        <Container>
+          <MobileAppInner>
+            <MobileAppContent>
+              <MobileTitle>BossBlog</MobileTitle>
+              <MobileText>
+                Mobil ilova bilan o’qish yana ham qulayroq
+              </MobileText>
+              <MobileBtns>
+                <DownBtnApple />
+                <DownBtnPlay />
+              </MobileBtns>
+            </MobileAppContent>
+            <MobileAppImg />
+          </MobileAppInner>
+        </Container>
+      </MobileAppWrapper>
+    </>
   );
 }
 
--- src/components/About/style.js
+++ src/components/About/style.js
@@ -3,6 +3,10 @@ import { globalContainer } from "../../root/global";
 import { ReactComponent as headerBg } from "../../assets/images/header_bg.svg";
 import uzbMap from "../../assets/images/uzb_map.png";
 
+import appleImg from "../../assets/images/mobile_apple_img.png";
+import playImg from "../../assets/images/mobile_playmarket_img.png";
+import mobileImg from "../../assets/images/mobile-img.png";
+
 export const OuterContainer = styled.div`
   background-color: #fff;
   min-height: 100vh;
@@ -152,5 +156,150 @@ export const AboutContentBtn = styled.button`
   }
   &:active {
     transform: translateY(0);
+  }
+`;
+
+// Carousel
+
+export const CarouserSectionWrapper = styled.div`
+  padding: 80px 0;
+`;
+
+// Mobile App
+
+export const MobileAppWrapper = styled.div`
+  padding: 62px 0;
+  background-color: #e8d7ff;
+  height: 320px;
+  overflow: hidden;
+  @media (max-width: 950px) {
+    height: 400px;
+  }
+  @media (max-width: 800px) {
+    height: 440px;
+  }
+  @media (max-width: 650px) {
+    height: auto;
+  }
+`;
+
+export const MobileAppInner = styled.div`
+  display: flex;
+  justify-content: space-between;
+  @media (max-width: 800px) {
+    align-items: center;
+  }
+  @media (max-width: 650px) {
+    flex-wrap: wrap;
+  }
+`;
+
+export const MobileAppContent = styled.div`
+  @media (max-width: 650px) {
+    width: 100%;
+  }
+`;
+
+export const MobileTitle = styled.h3`
+  color: #000;
+  font-size: 36px;
+  font-family: Inter;
+  font-weight: 500;
+  line-height: 48px;
+  margin-bottom: 20px;
+  @media (max-width: 950px) {
+    margin-top: 50px;
+  }
+  @media (max-width: 800px) {
+    margin-top: 0;
+  }
+`;
+
+export const MobileText = styled.p`
+  color: #000;
+  font-size: 20px;
+  font-family: Inter;
+  line-height: 28px;
+  margin-bottom: 48px;
+`;
+
+export const MobileBtns = styled.div`
+  display: flex;
+  align-items: center;
+  @media (max-width: 650px) {
+    justify-content: center;
+  }
+`;
+
+export const DownBtnApple = styled.button`
+  width: 156px;
+  height: 52px;
+  border-radius: 8px;
+  border: 1px solid #e8e8e8;
+  background: #fff;
+  box-shadow: 0px 1px 1px 0px rgba(0, 0, 0, 0.06);
+  background-image: url("${appleImg}");
+  background-repeat: no-repeat;
+  background-position: center center;
+  margin-right: 16px;
+  cursor: pointer;
+  transition: transform 0.2s ease;
+  &:hover {
+    transform: translateY(-2px);
+  }
+  &:active {
+    transform: translateY(0);
+  }
+  @media (max-width: 650px) {
+    width: 114px;
+    height: 38px;
+    background-size: 90px;
+  }
+`;
+
+export const DownBtnPlay = styled.button`
+  width: 156px;
+  height: 52px;
+  border-radius: 8px;
+  border: 1px solid #e8e8e8;
+  background: #fff;
+  box-shadow: 0px 1px 1px 0px rgba(0, 0, 0, 0.06);
+  background-image: url("${playImg}");
+  background-repeat: no-repeat;
+  background-position: center center;
+  cursor: pointer;
+  transition: transform 0.2s ease;
+  &:hover {
+    transform: translateY(-2px);
+  }
+  &:active {
+    transform: translateY(0);
+  }
+  @media (max-width: 650px) {
+    width: 114px;
+    height: 38px;
+    background-size: 90px;
+  }
+`;
+
+export const MobileAppImg = styled.div`
+  width: 345px;
+  height: 687px;
+  background-image: url("${mobileImg}");
+  background-repeat: no-repeat;
+  background-size: cover;
+  @media (max-width: 950px) {
+    width: 300px;
+    height: 600px;
+  }
+  @media (max-width: 800px) {
+    width: 173px;
+    height: 344px;
+  }
+  @media (max-width: 650px) {
+    order: -1;
+    margin-bottom: 32px;
+    margin-right: auto;
+    margin-left: auto;
   }
 `;
