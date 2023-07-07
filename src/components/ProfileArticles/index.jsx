f3ff537cbd8c99b7eaf9b630aa10e929945ea3b6
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-07-07 09:41:53 +0500

    Fis btns res to mobile

--- src/components/ProfileArticles/index.jsx
+++ src/components/ProfileArticles/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import LoadMoreButton from "../LoadMore";
 import ListArticle from "./List";
 import {
+  AddArticle,
   BtnsWrapper,
   Container,
   ListArticleWrapper,
@@ -10,13 +11,20 @@ import {
   SavedBtn,
 } from "./style";
 
+import { ReactComponent as Plus } from "../../assets/icons/plus-gray.svg";
+
 function ProfileArticles() {
   return (
     <OuterContainer>
       <Container>
         <BtnsWrapper>
+          <AddArticle>
+            <Plus style={{ marginRight: "8px" }} />
+            Yangi maqola chop etish
+          </AddArticle>
           <SavedBtn active="true">Saqlanganlar</SavedBtn>
           <SavedBtn>Oxirgi o’qilganlar</SavedBtn>
+          <SavedBtn>Moderatsiyaga yuborilganlar</SavedBtn>
         </BtnsWrapper>
         <ListArticleWrapper>
           <ListArticle />
--- src/components/ProfileArticles/style.js
+++ src/components/ProfileArticles/style.js
@@ -10,7 +10,12 @@ export const Container = styled.div`
 `;
 
 export const BtnsWrapper = styled.div`
-  padding: 48px 0;
+  padding-top: 48px;
+  display: flex;
+  flex-wrap: wrap;
+  @media (max-width: 880px) {
+    justify-content: center;
+  }
 `;
 
 export const SavedBtn = styled.button`
@@ -22,6 +27,7 @@ export const SavedBtn = styled.button`
   font-weight: 500;
   line-height: 24px;
   padding: 8px 12px;
+  margin-bottom: 48px;
   border-radius: 8px;
   background-color: ${({ active }) => (active ? "var(--light-blue)" : "#fff")};
   outline: none;
@@ -37,6 +43,9 @@ export const SavedBtn = styled.button`
   }
   &:active {
     transform: translateY(0);
+  }
+  @media (max-width: 880px) {
+    margin-bottom: 30px;
   }
   @media (max-width: 340px) {
     font-size: 15px;
@@ -57,5 +66,36 @@ export const MoreBtnWrapper = styled.div`
   padding-bottom: 80px;
   @media (max-width: 600px) {
     padding-bottom: 40px;
+  }
+`;
+
+export const AddArticle = styled.button`
+  background: #fff;
+  padding: 8px 12px;
+  border-radius: 8px;
+  border: 1px solid #e8e8e8;
+  box-shadow: 0px 1px 1px 0px rgba(0, 0, 0, 0.06);
+  display: flex;
+  align-items: center;
+  color: var(--dark-black);
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 1.1;
+  margin-right: 16px;
+  cursor: pointer;
+  margin-bottom: 48px;
+  transition: 0.2s linear;
+  @media (max-width: 880px) {
+    margin-bottom: 30px;
+  }
+  @media (max-width: 340px) {
+    font-size: 15px;
+    padding: 6px 10px;
+  }
+  &:hover {
+    transform: translateY(-2px);
+  }
+  &:active {
+    transform: translateY(0);
   }
 `;
