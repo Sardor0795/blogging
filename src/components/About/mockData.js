1c6ba1e61579ecb4550ac039d3e5b40f413b3ff3
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-07-15 12:01:20 +0500

    set new imgs to mock carousel

--- src/components/About/mockData.js
+++ src/components/About/mockData.js
@@ -1,18 +1,22 @@
 import manImg from "../../assets/images/carousel-img-1.jpg";
 import girlImg from "../../assets/images/carousel-img-2.jpg";
+import girlImg2 from "../../assets/images/carousel-img-3.jpg";
+import girlImg3 from "../../assets/images/carousel-img-4.jpg";
+import girlImg5 from "../../assets/images/carousel-img-6.jpg";
+import manImg2 from "../../assets/images/carousel-img-7.jpg";
 
 export const mockData = [
   { id: 1, img: girlImg, title: "Sevara Nizamova" },
-  { id: 2, img: manImg, title: "Sevara Nizamova" },
-  { id: 3, img: girlImg, title: "Sevara Nizamova" },
-  { id: 4, img: manImg, title: "Sevara Nizamova" },
-  { id: 5, img: girlImg, title: "Sevara Nizamova" },
-  { id: 6, img: manImg, title: "Sevara Nizamova" },
+  { id: 2, img: girlImg2, title: "Yael Shelbia" },
+  { id: 3, img: manImg2, title: "Rowan Atkinson" },
+  { id: 4, img: girlImg3, title: "Sevara Nizamova" },
+  { id: 5, img: girlImg2, title: "Yael Shelbia" },
+  { id: 6, img: girlImg5, title: "Thylane Blondeau" },
   { id: 7, img: girlImg, title: "Sevara Nizamova" },
   { id: 8, img: manImg, title: "Sevara Nizamova" },
-  { id: 9, img: girlImg, title: "Sevara Nizamova" },
-  { id: 10, img: manImg, title: "Sevara Nizamova" },
+  { id: 9, img: girlImg3, title: "Sevara Nizamova" },
+  { id: 10, img: girlImg5, title: "Thylane Blondeau" },
   { id: 11, img: girlImg, title: "Sevara Nizamova" },
-  { id: 12, img: manImg, title: "Sevara Nizamova" },
-  { id: 13, img: girlImg, title: "Sevara Nizamova" },
+  { id: 12, img: girlImg3, title: "Sevara Nizamova" },
+  { id: 13, img: girlImg2, title: "Yael Shelbia" },
 ];
