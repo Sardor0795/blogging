08bd6520ec9b3e3c926db78e2bc7743e5ab553f8
topilovasilbek <asilbektback@gmail.com>
2023-07-17 13:07:06 +0500

    unauthorized

--- src/components/AuthDetector/index.jsx
+++ src/components/AuthDetector/index.jsx
@@ -5,7 +5,7 @@ function AuthDetector({ auth: authCom, noauth: noAuthCom }) {
   const [auth, setAuth] = useState("loading");
 
   useEffect(() => {
-    setAuth(true);
+    setAuth(false);
   }, []);
 
   return auth === "loading" ? (
